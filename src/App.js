fd471de90e18cd3bda6cae340eee843232026fcc
KhoaMilan97 <KhoaMilan97>
2020-10-06 08:16:11 +0700

    updated application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import JumbotronContainer from "./containers/jumbotron";
-import FooterContainer from "./containers/footer";
-import FaqsContainer from "./containers/faqs";
+import { Home, Signin, Signup, Browse } from "./pages";
+
+import * as ROUTES from "./constants/routes";
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
 export default App;
 
-// 2h20p00s
+// 3h21p20s
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,18 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiXuiy4BhfMRaypXi9zFza3j_9fvglZUE",
+  authDomain: "twitter-clone-97045.firebaseapp.com",
+  databaseURL: "https://twitter-clone-97045.firebaseio.com",
+  projectId: "twitter-clone-97045",
+  storageBucket: "twitter-clone-97045.appspot.com",
+  messagingSenderId: "603625446328",
+  appId: "1:603625446328:web:900789139bc67d93dd0d4d",
+  measurementId: "G-DF6776MGDZ",
+};
+
+Firebase.initializeApp(firebaseConfig);
+
+export { firebase };
